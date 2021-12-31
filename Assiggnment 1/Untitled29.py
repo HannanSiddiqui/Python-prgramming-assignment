4c032c002212cdda703aeb311a69602ebcd560a0
Hannan siddiqui <Hannansiddiqui44@gmail.com>
2022-01-01 03:07:47 +0500

    python work

---
+++ Assiggnment 1/Untitled29.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+#Code 1
+
+print("Twinkle, twinkle, little star")
+print("        How I wonder what you are")
+print("                   Up above the world so high")
+print("                   Like a diamond in the sky")
+print("Twinkle, twinkle, little star")
+print("        How I wonder what you are")
+
+
+# In[ ]:
+
+
+
+
+
+# In[9]:
+
+
+#Code 2
+
+import sys
+print("The python version i am using is : ",sys.version)
+
+
+# In[ ]:
+
+
+
+
+
+# In[3]:
+
+
+#code 3
+
+from datetime import datetime
+today= datetime.today()
+print("the current date and time is",today)
+
+
+# In[8]:
+
+
+#code 4
+
+from math import pi
+r= float(input("enter value to find radius"))
+print("entered radius",str(r)+" is "+str(pi*r**2))
+
+
+# In[11]:
+
+
+#Code 5
+
+frtname = input("Enter First Name : ")
+lstname = input("Enter Last Name : ")
+print (lstname + " " + frtname);
+
+
+# In[ ]:
+
+
+
+
+
+# In[6]:
+
+
+
+
+
+# In[7]:
+
+
+#Code 6
+
+s = int(input("enter number1 :"))
+k = int(input("enter number2 :"))
+sum=s+k
+print("sum",sum)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
